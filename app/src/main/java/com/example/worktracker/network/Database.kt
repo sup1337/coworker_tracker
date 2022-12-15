bfbe9ed5614fbba9713ac107840d5dfee9cf5b01
sup1337 <64952429+sup1337@users.noreply.github.com>
2022-12-15 15:09:12 +0200

    logins ?!

--- app/src/main/java/com/example/worktracker/network/Database.kt
+++ app/src/main/java/com/example/worktracker/network/Database.kt
@@ -2,5 +2,8 @@ package com.example.worktracker.network
 
 data class Database(
     val host: String = "http://192.168.43.108//",
+    //coworkewrtracker.mysql.database.azure.com
+    //myadmin@coworkewrtracker
+    //port 3306
     val databaseName: String = "loginregister/"
 )
